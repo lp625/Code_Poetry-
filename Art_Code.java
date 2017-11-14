8b3e66faa66507228cd529003597d48bdd7eef23
lp625 <31662492+lp625@users.noreply.github.com>
2017-11-14 02:09:10 -0500

    Add files via upload

---
+++ Art_Code.java
@@ -0,0 +1,94 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package art_code;
+
+/**
+ *
+ * @author Lareb
+ */
+public class Art_Code {
+
+    
+   public static void main(String[]args) throws Exception {
+{
+System.out.println("This is a poem by Lareb");
+System.out.println("It is called The Next Move");
+System.out.println("Enjoy!");
+String [] phrases = new String [8];
+phrases[0] = "Capture the moment";
+phrases[1] = "Don’t let it slip";
+phrases[2] = "Let life take you a bliss ";
+phrases[3] = "Enjoy the time";
+phrases[4] = "While you are alive ";
+phrases[5] = "For all you know ";
+phrases[6] = "This could be";
+phrases[7] = "Your last throw";
+
+
+
+System.out.println();
+System.out.println();
+System.out.println(phrases[0]);
+try {
+    Thread.sleep(4000);                 //1000 milliseconds is one second.
+} catch(InterruptedException ex) {
+    Thread.currentThread().interrupt();
+}
+System.out.println(phrases[1]);
+try {
+    Thread.sleep(4000);                 //1000 milliseconds is one second.
+} catch(InterruptedException ex) {
+    Thread.currentThread().interrupt();
+}
+System.out.println(phrases[2]);
+try {
+    Thread.sleep(4000);                 //1000 milliseconds is one second.
+} catch(InterruptedException ex) {
+    Thread.currentThread().interrupt();
+}
+System.out.println(phrases[3]);
+try {
+    Thread.sleep(4000);                 //1000 milliseconds is one second.
+} catch(InterruptedException ex) {
+    Thread.currentThread().interrupt();
+}
+System.out.println(phrases[4]);
+try {
+    Thread.sleep(4000);                 //1000 milliseconds is one second.
+} catch(InterruptedException ex) {
+    Thread.currentThread().interrupt();
+}
+System.out.println(phrases[5]);
+try {
+    Thread.sleep(4000);                 //1000 milliseconds is one second.
+} catch(InterruptedException ex) {
+    Thread.currentThread().interrupt();
+}
+System.out.println(phrases[6]);
+try {
+    Thread.sleep(4000);                 //1000 milliseconds is one second.
+} catch(InterruptedException ex) {
+    Thread.currentThread().interrupt();
+}
+System.out.println(phrases[7]);
+try {
+    Thread.sleep(4000);                 //1000 milliseconds is one second.
+} catch(InterruptedException ex) {
+    Thread.currentThread().interrupt();
+}
+
+
+}
+   }
+}
+
+   
+
+
+
+
+
+ 
